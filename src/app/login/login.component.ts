6a50897165e99ed08d68574f0eb343c1f2336699
Ramon <WeeziMonkey@gmail.com>
2023-10-19 11:18:07 +0200

    Working test version of getting email and jwt via login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {SocialAuthService} from "@abacritt/angularx-social-login";
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import jwtDecode from "jwt-decode";
 
 @Component({
   selector: 'app-login',
@@ -8,17 +11,33 @@ import {SocialAuthService} from "@abacritt/angularx-social-login";
 })
 export class LoginComponent {
 
-  constructor(private authService: SocialAuthService) {
-  }
+  private loginUrl: string = environment.backendUrl + "/login"
+
+  constructor(private authService: SocialAuthService, private http: HttpClient) {}
 
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
-      this.login(user.email, user.idToken);
+      this.login(user.idToken);
     });
   }
 
-  login(email: string, idToken: string) {
+  login(idToken: string) {
+    this.http.post(this.loginUrl, idToken, {responseType: 'text'}).subscribe({
+      next: jwt => {
+        console.log(jwt);
+        this.test(jwt);
+      },
+      error: error => {
+        console.error('There was an error: ',  error);
+      }
+    });
+  }
 
+  test(jwt: string) {
+    const decoded: any = jwtDecode(jwt);
+    console.log("Token info: ");
+    console.log(decoded);
+    console.log("Email: " + decoded.sub);
   }
 
 }
