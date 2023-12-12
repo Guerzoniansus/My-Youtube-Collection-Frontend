8cd580680d04d06117a02579d52c2d04ac92db6c
Ramon <WeeziMonkey@gmail.com>
2023-12-12 18:59:28 +0100

    All unit tests pass

--- src/app/component/home/video-editor/video-editor.component.spec.ts
+++ src/app/component/home/video-editor/video-editor.component.spec.ts
@@ -1,18 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VideoEditorComponent } from './video-editor.component';
+import {TagService} from "../../../service/tag.service";
+import {VideoService} from "../../../service/video.service";
+import {YoutubeService} from "../../../service/youtube.service";
 
 describe('VideoEditorComponent', () => {
   let component: VideoEditorComponent;
-  let fixture: ComponentFixture<VideoEditorComponent>;
+  let tagService: jasmine.SpyObj<TagService>;
+  let videoService: jasmine.SpyObj<VideoService>;
+  let youtubeService: jasmine.SpyObj<YoutubeService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [VideoEditorComponent]
-    });
-    fixture = TestBed.createComponent(VideoEditorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    tagService = jasmine.createSpyObj("TagService", ["getTags", "createAndSaveTags"]);
+    videoService = jasmine.createSpyObj("VideoService", ["createVideo"]);
+    youtubeService = jasmine.createSpyObj("YoutubeService", ["getCodeFromUrl", "getVideoInfo"])
+    component = new VideoEditorComponent(tagService, videoService, youtubeService);
   });
 
   it('should create', () => {
